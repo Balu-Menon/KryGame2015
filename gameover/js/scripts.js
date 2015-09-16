5eb6ecf0b7b29b66a387f7372f06d9d8f44751ed
anilvsajith <anilvsajith@gmail.com>
2015-09-16 21:07:06 +0530

    Final Commit for Kryptos :*

---
+++ gameover/js/scripts.js
@@ -0,0 +1,87 @@
+/*
+ Author: Ukieweb
+ Template: Rocket (Coming Soon)
+ Version: 1.0
+ URL: http://themeforest.net/user/UkieWeb
+ */
+
+
+$(document).ready(function(){
+
+    "use strict";
+
+
+/*
+ ----------------------------------------------------------------------
+ Preloader
+ ----------------------------------------------------------------------
+ */
+    $(".loader").delay(400).fadeOut();
+    $(".animationload").delay(400).fadeOut("fast");
+
+
+/*
+ ----------------------------------------------------------------------
+ Nice scroll
+ ----------------------------------------------------------------------
+ */
+    $("html").niceScroll({
+        cursorcolor: '#fff',
+        cursoropacitymin: '0',
+        cursoropacitymax: '1',
+        cursorwidth: '2px',
+        zindex: 999999,
+        horizrailenabled: false,
+        enablekeyboard: false
+    });
+
+
+/*
+ ----------------------------------------------------------------------
+ Watch
+ ----------------------------------------------------------------------
+ */
+    if($.find('#watch')[0]) {
+
+        $('#watch').countDown({
+            targetDate: {
+                'day': 		17,
+                'month': 	9,
+                'year': 	2015,
+                'hour': 	10,
+                'min': 		0,
+                'sec': 		0
+            },
+            omitWeeks: true
+        });
+        //enter the count down date using the format year, month, day, time: hour, min, sec
+
+    }
+
+
+
+
+
+
+}); // End $(document).ready(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
